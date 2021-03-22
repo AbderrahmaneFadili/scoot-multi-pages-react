6ff8dac1c7e7d758a7ec44beec7d5f923ec65e1c
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-03-22 11:57:34 +0100

    Adding SignUp & Footer

--- src/pages/Location/Location.jsx
+++ src/pages/Location/Location.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import WordMap from "./WordMap";
 import CityListing from "./CityListing";
+import SignUp from "../../components/SignUp/SignUp";
+import Footer from "../../components/Footer/Footer";
+
 const Location = () => {
   return (
     <main>
       <Header title="Location" />
       <WordMap />
       <CityListing />
+      <SignUp />
+      <Footer />
     </main>
   );
 };
